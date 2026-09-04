c2ef6637f96165e95d2a75be0e667d5c2159c71f
jayedmahi321-dot <jayedmahi321@gmail.com>
2026-09-04 20:38:19 +0600

    Add files via upload

---
+++ c data types.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int studentID=39;
+    float score=5.65;
+    char grade= 'A';
+
+    printf("%d\n",studentID);
+    printf("%f\n", score);
+    printf("%c\n", grade);
+    return 0;
+}
