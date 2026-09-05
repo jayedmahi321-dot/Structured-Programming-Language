1daf0f4309c47e53ecf3d1352e83bf4f47c4c3a8
jayedmahi321-dot <jayedmahi321@gmail.com>
2026-09-05 20:41:18 +0600

    Add files via upload

---
+++ c operators.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int itemPrice= 50;
+    int shippingCost= 10;
+    int sum;
+    sum = itemPrice + shippingCost;
+    printf("Total cos is %d", sum);
+    return 0;
+}
