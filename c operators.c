784291eb259adb7acfc858dd97088d04cdc8033b
jayedmahi321-dot <jayedmahi321@gmail.com>
2026-09-24 15:29:06 +0600

    Delete c operators.c

--- c operators.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int itemPrice= 50;
-    int shippingCost= 10;
-    int sum;
-    sum = itemPrice + shippingCost;
-    printf("Total cos is %d", sum);
-    return 0;
-}
